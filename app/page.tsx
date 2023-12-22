834956a6230b59b24bd192fa1b23689eb967b41d
VelizarDabov <velizardabov@gmail.com>
2023-12-22 23:39:09 +0200

    film rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import MoviesCarousels from "@/components/MoviesCarousels"
-
+import { movies } from "./data/Categories"
+type movies= {
+movies:movies[]
+} 
 export default function Home() {
   return (
     <main className=''> 
 
  <div className="flex flex-col space-y-2 xl-mt-48">
   <MoviesCarousels movies={[]} title='Upcomming'/>
-    {/* <MoviesCarusousels movies= {...} title='Upcomming'/> */}
-      {/* <MoviesCarusousels movies= {...} title='Upcomming'/> */}
+    <MoviesCarousels movies= {[]} title='Top Rated'/>
+      <MoviesCarousels movies= {[]} title='Upcomming'/>
  </div>
     </main>
   )
